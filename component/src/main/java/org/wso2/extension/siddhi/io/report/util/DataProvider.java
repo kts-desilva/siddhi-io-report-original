e65b70c43b9b7ddb8d13b8e754e5be87d9a174ad
kts-desilva <ktsdesilva.15@cse.mrt.ac.lk>
2018-12-05 10:48:04 +0530

    Add javadoc comments

--- component/src/main/java/org/wso2/extension/siddhi/io/report/util/DataProvider.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/report/util/DataProvider.java
@@ -27,16 +27,37 @@ import java.util.List;
  */
 public interface DataProvider {
 
+    /**
+     * This method will return the abstract columns created for the dynamic report builder
+     * @return the list of abstract columns created.
+     */
     public List<AbstractColumn> getColumns();
 
+    /**
+     * This method will return an abstract column for a given column name
+     * @param columnName the name of the abstract column
+     * @return abstract column created in the dynamic report builder for the given name
+     */
     public AbstractColumn getCategoryColumn(String columnName);
 
+    /**
+     * This method will return the series column created in the dynamic report builder
+     * @param columnName name of the series column
+     * @return the abstract series column in the dynamic report builder for the given name
+     */
     public AbstractColumn getSeriesColumn(String columnName);
 
+    /**
+     * This method will return the category column created in the dynamic report builder
+     * @return the abstract category column chosen from the created abstract columns
+     */
     public AbstractColumn getCategoryColumn();
 
+    /**
+     * This method will returm the series column created in the dynamic report builder
+     * @return the abstract series column chosen from the created abstract columns
+     */
     public AbstractColumn getSeriesColumn();
-
 
 }
 
